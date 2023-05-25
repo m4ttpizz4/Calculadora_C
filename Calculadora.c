ff153f64d5d2f0ae86650a07e0bb8fd536d3bad4
M4tt_Pizz4 <100164090+M4ttPizz4@users.noreply.github.com>
2023-05-25 09:13:54 -0300

    Update Calculadora.c

--- Calculadora.c
+++ Calculadora.c
@@ -7,90 +7,89 @@
 //Int main e locale:
 int main(void){
 setlocale(LC_ALL,"Portuguese");
-//Vari·veis:
+//Vari√°veis:
 int operacao_menu, operacao_basic, operacao_fg, operacao_form, operacao_extra, somar_1, somar_2, somar_r, subtrair_1, subtrair_2, subtrair_r, multiplicar_1, multiplicar_2, multiplicar_r, raiz_1, raiz_r, potencia_1, potencia_2, potencia_r, bhaskara_a, bhaskara_b, bhaskara_c, bhaskara_delta, bhaskara_r1, bhaskara_r2, quadrado_1, quadrado_2, quadrado_r, triangulo_base, triangulo_altura, triangulo_r; 
 float porcen_1, porcen_2, porcen_r, circulo_1, circulo_r, dividir_1, dividir_2, dividir_r;
-//InteraÁ„o:
+//Intera√ß√£o:
 printf("\n[Calculadora (V3.5)]\n\n");
-printf("[Desenvolvido por: Matt]\n[Github: M4ttPizz4]\n\n");
-printf("Selecione a operaÁ„o teclando o n˙mero correspondente e pressionando 'Enter':\n\n1.OperaÁıes b·sicas.\n2.Raiz Quadrada.\n3.PotÍncia.\n4.FÛrmula de Bh·skara.\n5.Porcentagem.\n6.Figuras geomÈtricas.\n0.FÛrmulas matem·ticas.\n\n");
+printf("Selecione a opera√ß√£o teclando o n√∫mero correspondente e pressionando 'Enter':\n\n1.Opera√ß√µes b√°sicas.\n2.Raiz Quadrada.\n3.Pot√™ncia.\n4.F√≥rmula de Bh√°skara.\n5.Porcentagem.\n6.Figuras geom√©tricas.\n0.F√≥rmulas matem√°ticas.\n\n");
 scanf("%i", &operacao_menu);
 printf("\n------------------------------------------------------------------------\n\n");
-	//OperaÁıes b·sicas:
+	//Opera√ß√µes b√°sicas:
 	if(operacao_menu == 1){
-		printf("Selecione qual operaÁ„o b·sica deseja realizar e pressione a tecla 'Enter':\n\n1.Somar.\n2.Subtrair.\n3.Multiplicar.\n4.Dividir.\n\n");
+		printf("Selecione qual opera√ß√£o b√°sica deseja realizar e pressione a tecla 'Enter':\n\n1.Somar.\n2.Subtrair.\n3.Multiplicar.\n4.Dividir.\n\n");
 		scanf("%i", &operacao_basic);
 		printf("\n------------------------------------------------------------------------\n\n");
 	}
 		//Somar:
 	if(operacao_basic == 1){
-		printf("Para comeÁar a soma, digite o primeiro n˙mero e em seguida aperte 'Enter':\n\n");
+		printf("Para come√ßar a soma, digite o primeiro n√∫mero e em seguida aperte 'Enter':\n\n");
 		scanf("%i", &somar_1);
-		printf("\nDigite o segundo n˙mero:\n\n");
+		printf("\nDigite o segundo n√∫mero:\n\n");
 		scanf("%i", &somar_2);
 		printf("\n------------------------------------------------------------------------\n\n");
-			somar_r = somar_1 + somar_2;	//C·lculo
-		printf("O resultado da soma È : %i \n", somar_r);
+			somar_r = somar_1 + somar_2;	//C√°lculo
+		printf("O resultado da soma √© : %i \n", somar_r);
 	}
 		//Subtrair:
 	if(operacao_basic == 2){
-		printf("Para comeÁar a subtraÁ„o, digite o primeiro n˙mero e em seguida aperte 'Enter':\n\n");
+		printf("Para come√ßar a subtra√ß√£o, digite o primeiro n√∫mero e em seguida aperte 'Enter':\n\n");
 		scanf("%i", &subtrair_1);
-		printf("\nDigite o segundo n˙mero:\n\n");
+		printf("\nDigite o segundo n√∫mero:\n\n");
 		scanf("%i", &subtrair_2);
 		printf("\n------------------------------------------------------------------------\n\n");
-			subtrair_r = subtrair_1 - subtrair_2;	//C·lculo
-		printf("O resultado da subtraÁ„o È : %i \n", subtrair_r);
+			subtrair_r = subtrair_1 - subtrair_2;	//C√°lculo
+		printf("O resultado da subtra√ß√£o √© : %i \n", subtrair_r);
 	}
-		//MultiplicaÁ„o:
+		//Multiplica√ß√£o:
 	if(operacao_basic == 3){
-		printf("Para comeÁar a multiplicaÁ„o, digite o primeiro n˙mero e em seguida aperte 'Enter':\n\n");
+		printf("Para come√ßar a multiplica√ß√£o, digite o primeiro n√∫mero e em seguida aperte 'Enter':\n\n");
 		scanf("%i", &multiplicar_1);
-		printf("\nDigite o segundo n˙mero:\n\n");
+		printf("\nDigite o segundo n√∫mero:\n\n");
 		scanf("%i", &multiplicar_2);
 		printf("\n------------------------------------------------------------------------\n\n");
-			multiplicar_r = multiplicar_1 * multiplicar_2;	//C·lculo
-		printf("O resultado da multiplicaÁ„o È : %i \n", multiplicar_r);
+			multiplicar_r = multiplicar_1 * multiplicar_2;	//C√°lculo
+		printf("O resultado da multiplica√ß√£o √© : %i \n", multiplicar_r);
 	}
-		//Divis„o:
+		//Divis√£o:
 	if(operacao_basic == 4){
-		printf("Para comeÁar a divis„o, digite o primeiro n˙mero e em seguida aperte 'Enter':\n\n");
+		printf("Para come√ßar a divis√£o, digite o primeiro n√∫mero e em seguida aperte 'Enter':\n\n");
 		scanf("%f", &dividir_1);
-		printf("\nDigite o segundo n˙mero:\n\n");
+		printf("\nDigite o segundo n√∫mero:\n\n");
 		scanf("%f", &dividir_2);
 		printf("\n------------------------------------------------------------------------\n\n");
-			dividir_r = dividir_1 / dividir_2;	//C·lculo
-		printf("O resultado da divis„o È : %f. \n", dividir_r);
+			dividir_r = dividir_1 / dividir_2;	//C√°lculo
+		printf("O resultado da divis√£o √© : %f. \n", dividir_r);
 		}
 	//Raiz Quadrada:
 	if(operacao_menu == 2){
-		printf("Para calcular a raiz, digite o n˙mero que desejar e aperte 'Enter':\n\n");
+		printf("Para calcular a raiz, digite o n√∫mero que desejar e aperte 'Enter':\n\n");
 		scanf("%i", &raiz_1);
 		printf("\n------------------------------------------------------------------------\n\n");
-			raiz_r = sqrt(raiz_1);	//C·lculo
-		printf("O resultado da raiz quadrada È : %i. \n", raiz_r);
+			raiz_r = sqrt(raiz_1);	//C√°lculo
+		printf("O resultado da raiz quadrada √© : %i. \n", raiz_r);
 	}
-	//PotÍncia:
+	//Pot√™ncia:
 	if(operacao_menu == 3){
-		printf("Para calcular a potÍncia, digite o n˙mero que desejar e aperte 'Enter':\n\n");
+		printf("Para calcular a pot√™ncia, digite o n√∫mero que desejar e aperte 'Enter':\n\n");
 		scanf("%i", &potencia_1);
-		printf("Digite o expoente do seu n˙mero:\n\n");
+		printf("Digite o expoente do seu n√∫mero:\n\n");
 		scanf("%i", &potencia_2);
 		printf("\n------------------------------------------------------------------------\n\n");
 			potencia_r = pow(potencia_1, potencia_2);
-		printf("O resultado da potenciaÁ„o È : %i. \n", potencia_r);
+		printf("O resultado da potencia√ß√£o √© : %i. \n", potencia_r);
 	}
-	//Bh·skara:
+	//Bh√°skara:
 	if(operacao_menu == 4){
-		printf("Para fazer o c·lculo da fÛrmula de bhaskara digite o n˙mero correspondente a 'A' e aperte 'Enter':\n\n");
+		printf("Para fazer o c√°lculo da f√≥rmula de bhaskara digite o n√∫mero correspondente a 'A' e aperte 'Enter':\n\n");
 		scanf("%d", &bhaskara_a);
 		printf("\nDigite o valor de 'B':\n\n");
 		scanf("%d", &bhaskara_b);
 		printf("\nDigite o valor de 'C':\n\n");
 		scanf("%d", &bhaskara_c);
-			bhaskara_delta = pow(bhaskara_b, 2) - 4*bhaskara_a*bhaskara_c;	//C·lculo de delta
-			bhaskara_r1 = -bhaskara_b+sqrt(bhaskara_delta)/(2*bhaskara_a);	//C·lculo de X1
-			bhaskara_r2 = -bhaskara_b-sqrt(bhaskara_delta)/(2*bhaskara_a);	//C·lculo de X2
+			bhaskara_delta = pow(bhaskara_b, 2) - 4*bhaskara_a*bhaskara_c;	//C√°lculo de delta
+			bhaskara_r1 = -bhaskara_b+sqrt(bhaskara_delta)/(2*bhaskara_a);	//C√°lculo de X1
+			bhaskara_r2 = -bhaskara_b-sqrt(bhaskara_delta)/(2*bhaskara_a);	//C√°lculo de X2
 		printf("\n------------------------------------------------------------------------\n\n");
 		printf("Utilizando raiz de delta positivo em X1 e raiz de delta negativo em X2, podemos adquirir o seguinte resultado:\nX1 = %d. \nX2 = %d. \n\n", bhaskara_r1, bhaskara_r2);
 	}
@@ -98,92 +97,91 @@ printf("\n----------------------------------------------------------------------
 	if(operacao_menu == 5){
 		printf("\nPara calcular a porcentagem, digite o valor que deseja ser o porcento 'Enter':\n\n");
 		scanf("%f", &porcen_1);
-		printf("\nDigite o valor de onde o porcento ir· retirar o resultado:\n\n");
+		printf("\nDigite o valor de onde o porcento ir√° retirar o resultado:\n\n");
 		scanf("%f", &porcen_2);
-			porcen_r = porcen_2*(porcen_1/100);		//C·lculo
+			porcen_r = porcen_2*(porcen_1/100);		//C√°lculo
 		printf("\n------------------------------------------------------------------------\n\n");
-		printf("O resultado desse c·lculo È: %f. \n", porcen_r);
+		printf("O resultado desse c√°lculo √©: %f. \n", porcen_r);
 	}
-	//¡reas de figuras geomÈtricas:
+	//√Åreas de figuras geom√©tricas:
 	if(operacao_menu == 6){
-		printf("\nSelecione a figura geomÈtrica que desejar para calcular sua ·rea:\n\n1.Quadrado ou ret‚ngulo.\n2.CÌrculo.\n3.Tri‚ngulo.\n\n");
+		printf("\nSelecione a figura geom√©trica que desejar para calcular sua √°rea:\n\n1.Quadrado ou ret√¢ngulo.\n2.C√≠rculo.\n3.Tri√¢ngulo.\n\n");
 		scanf("%i", &operacao_fg);
 	}
-		//Quadrado/Ret‚ngulo:
+		//Quadrado/Ret√¢ngulo:
 	if(operacao_fg == 1){
-		printf("\nPara continuar a operaÁ„o, informe o valor do lado de cima ou de baixo do quadrado/ret‚ngulo: _\n\n");
+		printf("\nPara continuar a opera√ß√£o, informe o valor do lado de cima ou de baixo do quadrado/ret√¢ngulo: _\n\n");
 		scanf("%i", &quadrado_1);
-		printf("\nInsira o valor do lado esquerdo ou direito do quadrado/ret‚ngulo: |\n\n");
+		printf("\nInsira o valor do lado esquerdo ou direito do quadrado/ret√¢ngulo: |\n\n");
 		scanf("%i", &quadrado_2);
-			quadrado_r = quadrado_1 * quadrado_2;	//C·lculo
+			quadrado_r = quadrado_1 * quadrado_2;	//C√°lculo
 		printf("\n------------------------------------------------------------------------\n\n");
-		printf("O valor da ·rea do quadrado/ret‚ngulo È: %i \n", quadrado_r);
+		printf("O valor da √°rea do quadrado/ret√¢ngulo √©: %i \n", quadrado_r);
 	}
-		//CÌrculo:
+		//C√≠rculo:
 		if(operacao_fg == 2){
-		printf("\nPara calcular a ·rea do cÌrculo, insira o valor do raio:\n\n");
+		printf("\nPara calcular a √°rea do c√≠rculo, insira o valor do raio:\n\n");
 		scanf("%f", &circulo_1);
-			circulo_r = 3.14 * pow(circulo_1, 2);	//C·lculo
+			circulo_r = 3.14 * pow(circulo_1, 2);	//C√°lculo
 		printf("\n------------------------------------------------------------------------\n\n");
-		printf("O valor da ·rea do cÌrculo È : %f \n", circulo_r);
+		printf("O valor da √°rea do c√≠rculo √© : %f \n", circulo_r);
 	}
-		//Tri‚ngulo:
+		//Tri√¢ngulo:
 		if(operacao_fg == 3){
-		printf("\nPara calcular a ·rea do tri‚ngulo, insira o valor da base:\n\n");
+		printf("\nPara calcular a √°rea do tri√¢ngulo, insira o valor da base:\n\n");
 		scanf("%i", &triangulo_base);
 		printf("\nInsira o valor da altura:\n\n");
 		scanf("%i", &triangulo_altura);
-			triangulo_r = (triangulo_base*triangulo_altura)/2;	//C·lculo
+			triangulo_r = (triangulo_base*triangulo_altura)/2;	//C√°lculo
 		printf("\n------------------------------------------------------------------------\n\n");
-		printf("O valor da ·rea do tri‚ngulo È : %i \n", triangulo_r);
+		printf("O valor da √°rea do tri√¢ngulo √© : %i \n", triangulo_r);
 		}
-	//FÛrmulas matem·ticas:
+	//F√≥rmulas matem√°ticas:
 		if(operacao_menu == 0){
-		printf("\nDigite o n˙mero correspondente a cada formula e pressione a tecla 'Enter':\n1.FÛrmula de Bh·skara.\n2.Relatividade.\n3.¡rea do quadrado/ret‚ngulo.\n4.CircunferÍncia.\n5.¡rea do Tri‚ngulo.\n6.Extras.\n\n");
+		printf("\nDigite o n√∫mero correspondente a cada formula e pressione a tecla 'Enter':\n1.F√≥rmula de Bh√°skara.\n2.Relatividade.\n3.√Årea do quadrado/ret√¢ngulo.\n4.Circunfer√™ncia.\n5.√Årea do Tri√¢ngulo.\n6.Extras.\n\n");
 		scanf("%i", &operacao_form);
 		printf("\n------------------------------------------------------------------------\n\n");
 		}
-		//FÛrmula de Bh·skara:
+		//F√≥rmula de Bh√°skara:
 		if(operacao_form == 1){
-		printf("delta = b≤ - 4*a*c\n\n-b +-raiz{delta}/2*a.\n");
+		printf("delta = b¬≤ - 4*a*c\n\n-b +-raiz{delta}/2*a.\n");
 		}
 		//Relatividade:
 		if(operacao_form == 2){
 		printf("E = m*c*2.\n");
 		}
-		//¡rea do quadrado/ret‚ngulo:
+		//√Årea do quadrado/ret√¢ngulo:
 		if(operacao_form == 3){
-		printf("¡rea = Base * Altura.\n");
+		printf("√Årea = Base * Altura.\n");
 		}
-		//CircunferÍncia:
+		//Circunfer√™ncia:
 		if(operacao_form == 4){
 		printf("C = 2*pi*r.\n");
 		}
-		//Tri‚ngulo:
+		//Tri√¢ngulo:
 		if(operacao_form == 5){
 		printf("A = base*altura/2.\n");
 		}
 			//Extras:
 		if(operacao_form == 6){
-		printf("Insira o n˙mero correspondente a opÁ„o desejada:\n\n1.Valor de pi.\n2.Gravidade do planeta terra.\n3.Gravidade da lua da terra.\n\n");
+		printf("Insira o n√∫mero correspondente a op√ß√£o desejada:\n\n1.Valor de pi.\n2.Gravidade do planeta terra.\n3.Gravidade da lua da terra.\n\n");
 		scanf("%i", &operacao_extra);
 		}
 				//Valor de Pi:
 		if(operacao_extra == 1){
-		printf("O valor de Pi È: 3,14159265358979323846Ö\n");
+		printf("O valor de Pi √©: 3,14159265358979323846¬Ö\n");
 		}
 				//Gravidade do planeta terra:
 		if(operacao_extra == 2){
-		printf("A gravidade da terra È: 9,807 m/s≤.\n");
+		printf("A gravidade da terra √©: 9,807 m/s¬≤.\n");
 		}
 				//Gravidade da lua:
 		if(operacao_extra == 3){
-		printf("A gravidade da lua È: 1,62 m/s≤.\n");
+		printf("A gravidade da lua √©: 1,62 m/s¬≤.\n");
 		}
-//FinalizaÁıes:
+//Finaliza√ß√µes:
 printf("\n------------------------------------------------------------------------\n\n");
-printf("|***Obrigada pela visita! Feche a aplicaÁ„o.<3***|\n\n");
 system("Pause");
 return 0;
 }
-//Fim do cÛdigo.
+//Fim do c√≥digo.
